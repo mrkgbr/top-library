8e93d6dc38622827ad478547bccd6f2fe3509fac
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-04 18:23:31 +0200

    Update functions and style Add create and delete function and buttons Add style to the cards and page

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-const myLibrary = [];
+// dummy content for testing
+const myLibrary = [
+  {
+    title: "The Da Vinci Code",
+    author: "Brown, Dan",
+    pages: 300,
+    read: false,
+  },
+  {
+    title: "The Da Vinci Code 2",
+    author: "Brown, Dan",
+    pages: 400,
+    read: true,
+  },
+  {
+    title: "The Da Vinci Code 3",
+    author: "Brown, Dan",
+    pages: 500,
+    read: false,
+  },
+  {
+    title: "The Da Vinci Code 4",
+    author: "Brown, Dan",
+    pages: 600,
+    read: true,
+  },
+];
 
 function Book(title, author, pages, read) {
   // the constructor...
@@ -8,31 +34,56 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  // do stuff here
-  myLibrary.push(new Book(title, author, pages, read));
-}
-
-function createBookCard() {
+function createBookCard(book) {
   // do stuff here
   const container = document.querySelector("#container");
-  const content = document.createElement("div");
+  const card = document.createElement("div");
   const title = document.createElement("h1");
-  content.classList.add("content");
-  title.textContent = "This is the title!";
-  container.appendChild(content);
-  content.appendChild(title);
+  const author = document.createElement("h2");
+  const pages = document.createElement("p");
+  const read = document.createElement("p");
+  const delBtn = document.createElement("button");
+  card.classList.add("card");
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+  read.textContent = book.read;
+  delBtn.textContent = "Delete";
+  delBtn.setAttribute("type", "button");
+  delBtn.addEventListener("click", () => {
+    const index = myLibrary.indexOf(book);
+    console.log(index);
+    myLibrary.splice(index, 1);
+    card.remove();
+    // container.innerHTML = "";
+    // myLibrary.forEach((remainingBooks) => {
+    //   createBookCard(remainingBooks);
+    // });
+  });
+  container.appendChild(card);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+  card.appendChild(delBtn);
 }
 
-addBookToLibrary("The Da Vinci Code", "Brown, Dan", 654, false);
-addBookToLibrary("The Da Vinci Code 2", "Brown, Dan", 654, false);
-addBookToLibrary("The Da Vinci Code 3", "Brown, Dan", 654, false);
+function addBookToLibrary(title, author, pages, read) {
+  // do stuff here
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createBookCard(newBook);
+}
 
-myLibrary.forEach((item) => {
-  console.log(item);
+myLibrary.forEach((book) => {
+  createBookCard(book);
 });
 
-console.log(myLibrary[0].title);
-
-createBookCard();
-createBookCard();
+const addBtn = document.getElementById("add");
+addBtn.addEventListener(
+  "click",
+  () => {
+    addBookToLibrary("One", "Two", 333, true);
+  },
+  false
+);
